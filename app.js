545c5c8c743e18003ae11540f082a4d45e5047f2
kbriannicolas <karaim.brian@gmail.com>
2022-12-30 17:58:05 -0300

    iniciando proyecto con node

---
+++ app.js
@@ -0,0 +1,9 @@
+const express=require('express');
+const path=require('path');
+const app= express();
+
+app.listen(3000,()=>console.log("LISTENING ON PORT 3000"));
+
+app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'./views/index.html')))
+
+app.use(express.static(path.resolve(__dirname,'./public')))
