08c2166326768dd27eca5842d0003ca50619f980
kbriannicolas <karaim.brian@gmail.com>
2023-03-09 16:24:02 -0300

    database products, sequelize init, crud products

---
+++ src/routes/products.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const multer= require('multer');
+const path= require('path');
+const validate= require('../../middlewares/validation')
+const routerMain = express.Router();
+
+const productsController= require('../controllers/productsController')
+
+//configurando storage
+const storage= multer.diskStorage({
+    destination: (req, file, callback) => {
+        let folder=path.join(__dirname,'../../public/img');
+        callback(null,folder);
+    },
+    filename: (req, file, callback)=> {
+        const imagename = Date.now() + path.extname(file.originalname)
+        callback(null,  imagename)
+    }
+})
+let upload= multer({storage})
+
+routerMain.get('/products', productsController.allproducts);
+
+routerMain.get('/product/:id', productsController.productDetail);
+routerMain.get('/product/:id', productsController.productFind);
+
+routerMain.get('/products/create', productsController.productCreate);
+routerMain.post('/products/create', upload.single('image'),validate, productsController.productStore);
+
+routerMain.get('/products/:id/edit', productsController.productEdit);
+routerMain.put('/products/:id/edit',upload.single('image'),validate,productsController.productUpdate);
+
+routerMain.delete('/products/:id', productsController.productDestroy); 
+
+
+module.exports = routerMain;
