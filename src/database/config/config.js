c5f95aefcd17663924e41ffcc90a4f933a8c2eb1
kbriannicolas <karaim.brian@gmail.com>
2023-03-14 14:51:48 -0300

    deploy database

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,10 +1,13 @@
+if (process.env.NODE_ENV!='production') {
+  require('dotenv').config()
+}
 module.exports={
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "productos_db",
-    "host": "localhost",
-    "dialect": "mysql"
+    "username": process.env.USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "dialect": "mysql",
   },
   "test": {
     "username": "root",
